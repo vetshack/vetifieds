3a32e08bfc5231cc3a288c42cf1d2b1940d28f38
Jane Fong <janeefongg@gmail.com>
2016-09-03 12:05:50 -0700

    [feature] post mentor

--- server/controllers/mentor.js
+++ server/controllers/mentor.js
@@ -3,7 +3,6 @@ let authHelper = require('../helpers/authHelper');
 let Mentor = require('../db/models/mentors');
 let Q = require('q');
 
-
 module.exports = (function() {
   let controller = ControllerPrototype.create({
     path: '/mentors'
@@ -15,30 +14,38 @@ module.exports = (function() {
   });
   
   router.post('/', function(req, res) {
-    console.log('inside post', Mentor);
 
-    // var postMentor = Q.nbind(User.findOne, User);
-    // findUser({username: reqUser.username})
-    //   .then(function (user) {
-    //     if (!user) {
-    //       next(new Error('User does not exist'));
-    //     } else {
-    //       return user.comparePasswords(reqUser.password)
-    //         .then(function(foundUser) {
-    //           console.log("foundUser", foundUser);
-    //           if (foundUser) {
-    //             var token = authHelper.generateToken(user)
-    //             res.json({token: token, user:user});
-    //           } else {
-    //             res.status(400).json({msg:"No User found"})
-    //           }
-    //         });
-    //     }
-    //   })
-    //   .fail(function (error) {
-    //     console.log("failed, line in /signin")
-    //     next(error);
-    //   });
+    if (!authHelper.isAuthenticated()) return res.status(401).send('User unauthorized');
+
+    req.assert('email', 'Email is not valid').isEmail();
+    req.assert('email', 'Email cannot be blank').notEmpty();
+    req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+    let errors = req.validationErrors();
+
+    if (errors) return res.status(400).send(errors);
+
+    let postMentor = Q.nbind(Mentor.create, Mentor);
+    postMentor({
+      type: req.body.type,
+      fullname: req.body.fullname,
+      username: req.body.username,
+      imageUrl: req.body.imageUrl,
+      location: req.body.location,
+      industry: req.body.industry,
+      unit: req.body.unit,
+      mos: req.body.mos,
+      phone: req.body.phone,
+      email: req.body.email
+    })
+      .then(function () {
+        console.log('post successful');
+        res.json({success: true});
+      })
+      .fail(function (error) {
+        console.log("failed, line 45 in /mentor")
+        next(error);
+      });
   });
 
   return controller;
