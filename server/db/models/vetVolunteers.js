a50563f4027f86091bb8e268c1bf81face84eb59
Ryan Morris <ryanmorris.j@gmail.com>
2016-09-03 09:08:13 -0700

    [Feature] Added vetVolunteers schema

---
+++ server/db/models/vetVolunteers.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const vetVolunteersSchema = mongoose.Schema({
+  name: String,
+  location: Number,
+  description: String,
+  okToContact: Boolean,
+  phone: String,
+  email: String
+});
+
+module.exports = vetVolunteersSchema;
