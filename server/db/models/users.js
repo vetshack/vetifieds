a3cd6128db8cf8b967d5fa5529b0868507d9f6dd
Justin Chen <justinpchen94@gmail.com>
2016-09-03 10:15:16 -0700

    [feature] add in user password hashing and comparing

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,4 +1,9 @@
-let mongoose = require('mongoose');
+let mongoose = require('mongoose'),
+    bcrypt   = require('bcrypt'),
+    Q        = require('q'),
+    saltRounds  = 10,
+    db = require('../db.js');
+
 
 let UserSchema = mongoose.Schema({
   fullname: String,
@@ -8,4 +13,39 @@ let UserSchema = mongoose.Schema({
   isVet: Boolean
 });
 
-module.exports = UserSchema;
+UserSchema.methods.comparePasswords = function (attempt) {
+  var defer = Q.defer();
+  var savedPassword = this.password;
+  bcrypt.compare(attempt, savedPassword, function (err, isMatch) {
+    if (err) {
+      defer.reject(err);
+    } else {
+      defer.resolve(isMatch);
+    }
+  });
+  return defer.promise;
+
+};
+
+UserSchema.pre('save', function (next) {
+
+  var user = this;
+
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      user.salt = salt;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
