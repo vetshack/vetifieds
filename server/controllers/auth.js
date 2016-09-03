2fd21e1b374560fff0ff23a2eae942319a31d436
Justin Chen <justinpchen94@gmail.com>
2016-09-02 22:54:11 -0700

    [feature] add req assertions

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 var ControllerPrototype = require('./controller.prototype');
+var authHelper = require('../helpers/authHelper');
 
 module.exports = (function() {
   var controller = ControllerPrototype.create({
@@ -7,21 +8,53 @@ module.exports = (function() {
   var router = controller.router;
   
   router.post('/login', function(req, res) {
-    let params = {
+    req.assert('email', 'Email is not valid').isEmail();
+    req.assert('email', 'Email cannot be blank').notEmpty();
+    req.assert('password', 'Password cannot be blank').notEmpty();
+    req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      return res.status(400).send(errors);
+    }
+
+    let user = {
       username: req.body.username || "",
       email: req.body.email || "",
       password: req.body.password
     }
 
-    res.send("we hit /login");
+    res.json(user);
+    // res.send("we hit /login");
   });
 
   router.post('/signup', function(req, res) {
-    let params = {
+
+    req.assert('name', 'Name cannot be blank').notEmpty();
+    req.assert('email', 'Email is not valid').isEmail();
+    req.assert('email', 'Email cannot be blank').notEmpty();
+    req.assert('password', 'Password must be at least 4 characters long').len(4);
+    req.sanitize('email').normalizeEmail({
+      remove_dots: false
+    });
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+      return res.status(400).send(errors);
+    }
+    
+    let user = {
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     }
+
+
+    //create user
+
+    //attach token
 
     res.send("we hit /signup");
   });
