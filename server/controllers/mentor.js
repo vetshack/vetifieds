ca5ca1719f1065d80b78de54c0d1e12173d01595
Jane Fong <janeefongg@gmail.com>
2016-09-03 11:26:26 -0700

    [setup] mentor route

--- server/controllers/mentor.js
+++ server/controllers/mentor.js
@@ -1,13 +1,44 @@
-var ControllerPrototype = require('./controller.prototype');
+let ControllerPrototype = require('./controller.prototype.js');
+let authHelper = require('../helpers/authHelper');
+let Mentor = require('../db/models/mentors');
+let Q = require('q');
+
 
 module.exports = (function() {
-  var controller = ControllerPrototype.create({
+  let controller = ControllerPrototype.create({
     path: '/mentors'
   });
-  var router = controller.router;
+  let router = controller.router;
 
   router.get('/', function(req, res) {
     res.send("we hit /mentors");
+  });
+  
+  router.post('/', function(req, res) {
+    console.log('inside post', Mentor);
+
+    // var postMentor = Q.nbind(User.findOne, User);
+    // findUser({username: reqUser.username})
+    //   .then(function (user) {
+    //     if (!user) {
+    //       next(new Error('User does not exist'));
+    //     } else {
+    //       return user.comparePasswords(reqUser.password)
+    //         .then(function(foundUser) {
+    //           console.log("foundUser", foundUser);
+    //           if (foundUser) {
+    //             var token = authHelper.generateToken(user)
+    //             res.json({token: token, user:user});
+    //           } else {
+    //             res.status(400).json({msg:"No User found"})
+    //           }
+    //         });
+    //     }
+    //   })
+    //   .fail(function (error) {
+    //     console.log("failed, line in /signin")
+    //     next(error);
+    //   });
   });
 
   return controller;
