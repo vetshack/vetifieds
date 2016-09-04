60ee5e4f848dbe93fa96c3eda2d13b0d62269ec1
Chris Kuh <kuh_chris@yahoo.com>
2016-09-03 20:13:46 -0700

    [style] updated color theme

--- client/pages/vetshack.home/home.controller.js
+++ client/pages/vetshack.home/home.controller.js
@@ -5,15 +5,20 @@ const HomeController = function(Home) {
 
   vm.mentorList = [{
     title: 'Receive on target guidance',
-    text: '...because they\'ve been down a similar path'
+    text: 'Because they\'ve been down a similar path'
   }, {
     title: 'Forge a valuable connection',
-    text: '...because they\'ve been down a similar path'
+    text: 'Build ongoing relationship with someone who understands how to readjust to civilian life.'
   }, {
-    title: 'Receive on target guidance',
-    text: '...because they\'ve been down a similar path'
+    title: 'Rewinding expereience',
+    text: 'Helping others develop, and succeed is invaluable.'
   }];
+  
+  vm.isActive = false;
 
+  vm.toggleActive = () => {
+    vm.isActive = !vm.isActive;
+  }
   vm.init = () => {
     Home.getCata()
       .then((data) => {
